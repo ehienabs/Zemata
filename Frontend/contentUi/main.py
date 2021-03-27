f3ffce539756c2827c3db3fdeaca053cc25f580a
Ehi Enabs <Enabs.ehi@gmail.com>
2021-03-27 11:45:56 +0100

    zemata frontend

---
+++ Frontend/contentUi/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return f"<h1>This baby serves content</h1>"
+
+if __name__ == '__main__':
+   
+    app.run()
